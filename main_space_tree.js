f33a1d2bc93332679586457ea9b7e57d1b37976f
zypang <pangzaiyu@163.com>
2019-11-07 22:41:24 +0800

    now can layout and animate

--- main_space_tree.js
+++ main_space_tree.js
@@ -3,8 +3,13 @@ if (!Array.prototype.last){
         return this[this.length - 1];
     };
 }
-
-let tree={'node_w':1,'node_y':1};
+const margin = {top: 20, right: 30, bottom: 30, left: 40},
+    width = 660 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+let oriData;
+let r;
+let tree={'node_w':1,'node_y':1,'node_r':20};
+tree.comp=(a,b)=>a.val<b.val;
 tree.sortChildren=function()
 {
     let comp=tree.comp;
@@ -14,11 +19,29 @@ tree.sortChildren=function()
     }
     sortC(tree.root);
 };
+tree.calValue=function () {
+    function calV(v) {
+        if(!v.children.length) {v.val=1;return;}
+        v.children.forEach(v=>calV(v));
+        v.val=v.children.reduce((prev,cur)=>{return {'val':prev.val+cur.val};}).val;
+    }
+    calV(tree.root);
+};
 const DIRECTION={
   'u': ['y',1],
     'd':['y',-1],
     'l':['x',-1],
     'r':['x',1]
+};
+tree.clone=function(root){
+    function cl(v) {
+        let children=[];
+        v.children.forEach(c=>children.push(cl(c)));
+        let new_v=Object.assign({},v);
+        new_v.children=children;
+        return new_v;
+    }
+    tree.root=cl(root);
 };
 let moveTree=function(v,d,l){
     v.p[DIRECTION[d][0]]+=DIRECTION[d][1]*l;
@@ -33,22 +56,19 @@ tree.filter=function(filter){
     else tree.root=undefined;
 };
 tree.calAbsPosition=function(){
-    let filter=tree.filter;
     function calXY(v) {
-      if(!filter(v)) return;
-      let children=v.children.filter(filter);
-      if(!children.length) { v.p = {'x': 1, 'y': 1, 'w': 1}; return; }
-      children.forEach(child=>calXY(child));
+      if(!v.children.length) { v.p = {'x': 1, 'y': 1, 'w': 1}; return; }
+      v.children.forEach(child=>calXY(child));
       let left=0,maxY=-1;//,max_h=-1;
-      children.forEach(child=>{
+      v.children.forEach(child=>{
          //child.p.x+=left;
-          moveTree(child,'r',left);
+         moveTree(child,'r',left);
          left+=child.p.w;
          maxY=child.p.y>maxY?child.p.y:maxY;
       });
-      children.forEach(v=>v.p.y=maxY);
+      v.children.forEach(v=>v.p.y=maxY);
       v.p={
-          'x':(children[0].p.x+children.last().p.x)/2,
+          'x':(v.children[0].p.x+v.children.last().p.x)/2,
           'y':maxY+1,
           'w':left
       };
@@ -56,42 +76,43 @@ tree.calAbsPosition=function(){
     calXY(tree.root);
 };
 tree.getVertices=function(){
-    let filter=tree.filter;
     let vs=[];
     let getV=(v,f)=>{
-        if(filter(v)) {
-            vs.push({'name': v.name,'val':v.val, 'p': v.p,'f':f});
-            v.children.filter(filter).forEach(c => getV(c,{'p':v.p}));
-        }
+        vs.push({'name': v.name,'val':v.val, 'p': v.p,'f':f});
+        v.children.forEach(c => getV(c,{'p':v.p}));
     };
-    getV(tree.root);
+    getV(tree.root,tree.root);
     return vs.sort((a,b)=>a.val<b.val);
 };
 tree.getEdges=function(){
-    let filter=tree.filter;
     let edges=[];
     let getE=v=>{
-        if(filter(v)) {
-            v.children.filter(filter).forEach(c => edges.push({'p1': v.p, 'p2': c.p}));
-            v.children.filter(filter).forEach(c => getE(c));
-        }
+        v.children.forEach(c => edges.push({'p1': v.p, 'p2': c.p}));
+        v.children.forEach(c => getE(c));
     };
     getE(tree.root);
     return edges;
 };
-let svgWidth=300,svgHeight=300;
+//let svgWidth=300,svgHeight=300;
 function update(filter) {
-    tree.filter=filter;
-    tree.calAbsPosition();
-    let vs=tree.getVertices();
-    //console.log(vs);
+    tree.clone(oriData[0]);
+    tree.filter(filter);
+    let vs=[],edges=[];
+    let x,y;
+    if(tree.root) {
+        tree.sortChildren();
+        tree.calAbsPosition();
+        vs = tree.getVertices();
+        edges=tree.getEdges();
+        //console.log(vs);
 
-    let maxX=vs.reduce((prev,cur)=>{
-        if(prev.p.x>cur.p.x) return {'p':{'x':prev.p.x}};
-        return {'p':{'x':cur.p.x}};
-    }).p.x;
-    let x=d3.scaleLinear().domain([0,maxX]).range([0,svgWidth]);
-    let y=d3.scaleLinear().domain([0,tree.root.p.y]).range([svgHeight,0]);
+        let maxX = vs.reduce((prev, cur) => {
+            if (prev.p.x > cur.p.x) return {'p': {'x': prev.p.x}};
+            return {'p': {'x': cur.p.x}};
+        }).p.x;
+        x = d3.scaleLinear().domain([1, maxX]).range([tree.node_r, width - tree.node_r]);
+        y = d3.scaleLinear().domain([1, tree.root.p.y]).range([height - tree.node_r, tree.node_r]);
+    }
 
     let circles=d3.select("#g_circles")
         .selectAll("circle")
@@ -105,12 +126,11 @@ function update(filter) {
     circles.enter().append("circle")
         .attr("cx",d=>x(d.f.p.x))
         .attr("cy",d=>y(d.f.p.y))
-        .attr("r",10)
+        .attr("r",d=>r(d.val))
         .transition().duration(500)
         .attr("cx",d=>x(d.p.x))
         .attr("cy",d=>y(d.p.y));},500);
 
-    let edges=tree.getEdges(filter);
     let lines=d3.select("#g_lines")
         .selectAll("line")
         .data(edges);
@@ -125,52 +145,29 @@ function update(filter) {
     lines.transition().duration(500)
         .attr("x1",d=>x(d.p1.x)).attr("y1",d=>y(d.p1.y))
         .attr("x2",d=>x(d.p2.x)).attr("y2",d=>y(d.p2.y));
+    setTimeout(()=>d3.selectAll("circle").on("click",d=>console.log(d.val)),500);
 
 }
-function showTree(data){
-    let filter=v=>true;
-    tree.root=data[0];
-    tree.filter=filter;
-    tree.comp=(a,b)=>a.val<b.val;
-    tree.sortChildren();
-    tree.calAbsPosition();
-    let vs=tree.getVertices();
-    console.log(vs);
-    let maxX=vs.reduce((prev,cur)=>{
-        if(prev.p.x>cur.p.x) return {'p':{'x':prev.p.x}};
-        return {'p':{'x':cur.p.x}};
-    }).p.x;
-    let x=d3.scaleLinear().domain([0,maxX]).range([0,svgWidth]);
-    let y=d3.scaleLinear().domain([0,tree.root.p.y]).range([svgHeight,0]);
-
+function main(data){
     d3.select("body")
         .append("svg")
-        .attr("width",svgWidth)
-        .attr("height",svgHeight);
+        .attr("width",width+margin.left+margin.right)
+        .attr("height",height+margin.top+margin.bottom);
 
     let svg=d3.select("svg");
-    svg.append("g").attr("id","g_lines");
-    svg.append("g").attr("id","g_circles");
+    svg.append("g").attr("id","g_lines")
+        .attr("transform",`translate(${margin.left},${margin.top}`);
+    svg.append("g").attr("id","g_circles")
+        .attr("transform",`translate(${margin.left},${margin.top}`);
 
-    d3.select("#g_circles")
-        .selectAll("circle")
-        .data(vs)
-        .enter().append("circle")
-        .attr("cx",d=>x(d.p.x))
-        .attr("cy",d=>y(d.p.y))
-        .attr("r",10);
-
-    let edges=tree.getEdges(filter);
-    svg.select("#g_lines")
-        .selectAll("line")
-        .data(edges)
-        .enter()
-        .append("line")
-        .attr("x1",d=>x(d.p1.x)).attr("y1",d=>y(d.p1.y))
-        .attr("x2",d=>x(d.p2.x)).attr("y2",d=>y(d.p2.y));
-    d3.selectAll("circle").on("click",d=>console.log(d.val));
-    //d3.select("#doi").on('change',(d,i,nodes)=>console.log(d3.select(nodes[i]).property("value")));
     d3.select("#doi").on('change',(d,i,nodes)=>update(v=>v.val>nodes[i].value));
 
+    let filter=v=>true;
+    oriData=data;
+    tree.root=oriData[0];
+    tree.calValue();
+    r = d3.scaleSqrt().domain([0, tree.root.val]).range([0, tree.node_r]);
+    update(filter);
+    //d3.select("#doi").on('change',(d,i,nodes)=>console.log(d3.select(nodes[i]).property("value")));
 }
-d3.json("data.json").then(showTree).catch(error=>console.log(error));
+d3.json("data.json").then(main).catch(error=>console.log(error));
